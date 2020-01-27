0b8ad7a9b029da1dd2155925e233a325ccaec625
Eduardo Pinheiro <38962010+eduardo-pinheiro@users.noreply.github.com>
2020-01-27 21:18:46 -0200

    start with git clone

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,46 @@
 import * as express from 'express'
-var app = express();
+import { Application } from 'express'
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+class App {
+    public app: Application
+    public port: number
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+    constructor(appInit: { port: number; middleWares: any; controllers: any; }) {
+        this.app = express()
+        this.port = appInit.port
+
+        this.middlewares(appInit.middleWares)
+        this.routes(appInit.controllers)
+        this.assets()
+        this.template()
+    }
+
+    private middlewares(middleWares: { forEach: (arg0: (middleWare: any) => void) => void; }) {
+        middleWares.forEach(middleWare => {
+            this.app.use(middleWare)
+        })
+    }
+
+    private routes(controllers: { forEach: (arg0: (controller: any) => void) => void; }) {
+        controllers.forEach(controller => {
+            this.app.use('/', controller.router)
+        })
+    }
+
+    private assets() {
+        this.app.use(express.static('public'))
+        this.app.use(express.static('views'))
+    }
+
+    private template() {
+        this.app.set('view engine', 'pug')
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`App listening on the http://localhost:${this.port}`)
+        })
+    }
+}
+
+export default App
